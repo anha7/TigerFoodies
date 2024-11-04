99e85ff1d675904fc3d4cb92831a340db0bc2c3a
Arika Hassan <ah5155@princeton.edu>
2024-11-04 13:48:01 -0500

    view card modal files

--- frontend/src/pages/ViewCards.js
+++ frontend/src/pages/ViewCards.js
@@ -7,6 +7,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './ViewCards.css'; // Import custom CSS file
 import matheyImage from './media/mathey.png';
+import ViewCardModal from './ViewCardModal'; // to view extended card info
 
 //----------------------------------------------------------------------
 
