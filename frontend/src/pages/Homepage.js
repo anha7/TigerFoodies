2cd72ee91d68f1a6b542d459b58c8f1c05e22874
sami-dalu <samiradwdalu@gmail.com>
2024-12-01 22:31:41 -0500

    added onclose debug statements to Homepage

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -18,27 +18,21 @@ import { io } from "socket.io-client";
 //----------------------------------------------------------------------
 
 // Connection to flask-socketio server
-const socket = io({
-    transports: ['websocket']
-  });
-
-  socket.on("disconnect", (reason, details) => {
-    console.log("reason")
-    console.log(reason)
-    console.log("details")
-    console.log(details)
-  });
-
-  socket.on("connect_error", (error) => {
-    if (socket.active) {
-      // temporary failure, the socket will automatically try to reconnect
-    } else {
-      // the connection was denied by the server
-      // in that case, `socket.connect()` must be manually called in order to reconnect
-      console.log("connect error")
-      console.log(error.message);
+var socket;
+try {
+    socket = io("ws://tigerfoodies.com", {
+        transports: ['websocket']
+    });
+}
+catch (error) {
+    console.error(error)
+} 
+  socket.onclose = (event) => {
+    console.log('WebSocket connection closed:', event);
+    if (event.code !== 1000) {
+      console.error('Connection closed unexpectedly');
     }
-  });
+  };
   
 //----------------------------------------------------------------------
 
